4b7d197353edee6a95d7f841650156dd97e4d7ab
Prabin Bastakoti <101822704+PrabinBastakoti@users.noreply.github.com>
2023-07-23 17:40:37 +0545

    Design Navbar || Add footer

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import initialLoad from "./functions/initialLoad";
+import { toggleTasks } from "./pages/homepage";
 import "./styles/homepage.css";
 
 initialLoad();
 
 const dropdownTasks = document.querySelector(".tasks");
 
-dropdownTasks.addEventListener("click", (e) => {
-  console.log(e.target);
-  console.log("Hello This is right target(i.e. Tasks dropdown item");
+dropdownTasks.addEventListener("click", () => {
+  toggleTasks();
 });
--- src/pages/homepage.js
+++ src/pages/homepage.js
@@ -1,6 +1,8 @@
 import HeadingLogo from "../assets/favicon.svg";
 
-import task from "../assets/downarrow.png";
+import downarrowIcon from "../assets/downarrow.png";
+
+import rightarrowIcon from "../assets/rightarrow.png";
 
 import today from "../assets/today.png";
 
@@ -9,6 +11,10 @@ import week from "../assets/thisweek.png";
 import month from "../assets/thismonth.png";
 
 import projects from "../assets/projects.png";
+
+import taskIcon from "../assets/tasks.png";
+
+import githubLogo from "../assets/github.png";
 
 const functions = (() => {
   const addImage = () => {
@@ -20,8 +26,11 @@ const functions = (() => {
   };
 
   const addIcons = () => {
-    const tasksIcon = document.getElementById("tasksIcon");
-    tasksIcon.src = task;
+    const arrowIcons = document.getElementById("arrowIcon");
+    arrowIcons.src = downarrowIcon;
+
+    const taskIcons = document.getElementById("tasksIcon");
+    taskIcons.src = taskIcon;
 
     const todayIcon = document.getElementById("todayIcon");
     todayIcon.src = today;
@@ -34,6 +43,9 @@ const functions = (() => {
 
     const projectIcon = document.getElementById("projectIcon");
     projectIcon.src = projects;
+
+    const githubIcon = document.getElementById("githubLogo");
+    githubIcon.src = githubLogo;
   };
 
   return { addImage, addIcons };
@@ -45,4 +57,20 @@ const homepage = () => {
   functions.addIcons();
 };
 
+const toggleTasks = () => {
+  const nestedItems = document.querySelectorAll(".nested");
+  const arrowIcons = document.getElementById("arrowIcon");
+
+  nestedItems.forEach((item) => {
+    item.classList.toggle("active");
+
+    if (item.classList.contains("active")) {
+      arrowIcons.src = rightarrowIcon;
+    } else {
+      arrowIcons.src = downarrowIcon;
+    }
+  });
+};
+
 export default homepage;
+export { toggleTasks };
